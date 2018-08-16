dfb144f7004da4d7ae46a49ec957f8dd34910259
Luciano Perezzini <lperezzini@gmail.com>
2018-08-16 13:00:16 -0300

    Define 'authors' and 'bo_nacional' pipelines

--- crawler/pipelines.py
+++ crawler/pipelines.py
@@ -5,7 +5,47 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+from database import DB
+from datetime import date
 
-class CrawlerPipeline(object):
-    def process_item(self, item, spider):
-        return item
+class AuthorPipeline(object):
+	collection_name = 'authors'
+
+	def __init__(self):
+		self.mongo_client = DB()
+		self.db = self.mongo_client.import_db('test')
+
+		self.items = list()
+
+	def close_spider(self, spider):
+		document = {
+			'date': str(date.today()),
+			'authors': list(map(lambda a: dict(a), self.items))
+		}
+		self.mongo_client.insert_one(self.collection_name, document)
+		self.mongo_client.close()
+
+	def process_item(self, item, spider):
+		self.items.append(item)
+		return item
+
+class BONacionalPipeline(object):
+	collection_name = 'bo_nacional'
+
+	def __init__(self):
+		self.mongo_client = DB()
+		self.db = self.mongo_client.import_db('test')
+
+		self.items = list()
+
+	def close_spider(self, spider):
+		document = {
+			'date': str(date.today()),
+			'norms': list(map(lambda n: dict(n), self.items))
+		}
+		self.mongo_client.insert_one(self.collection_name, document)
+		self.mongo_client.close()
+
+	def process_item(self, item, spider):
+		self.items.append(item)
+		return item
