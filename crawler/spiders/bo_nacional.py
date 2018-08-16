6249073ff0285515404854d4207b8a4f961b5e36
Luciano Perezzini <lperezzini@gmail.com>
2018-08-16 13:00:34 -0300

    Use item

--- crawler/spiders/bo_nacional.py
+++ crawler/spiders/bo_nacional.py
@@ -1,18 +1,17 @@
 import scrapy
 from scrapy_splash import SplashRequest
 from bs4 import BeautifulSoup
+from crawler.items import Norma
 
 class BONacional(scrapy.Spider):
-    name = "bo_nacional"
+    name = 'bo_nacional'
+    #allowed_domains = 'boletinoficial.gob.ar' FIXME
 
     def start_requests(self):
         url = 'https://www.boletinoficial.gob.ar/'
         yield SplashRequest(url=url, callback=self.parse)
 
     def parse(self, response):
-    	print('========== PARSING =========')
-    	print(response.text)
-    	print('========== END PARSING =========')
     	norms = response.css('div#PorCadaNorma')
     	norm_items = norms.css('div.itemsection')
     	norm_urls = norm_items.css('h3 > a::attr(href)').extract()
@@ -22,13 +21,15 @@ class BONacional(scrapy.Spider):
     		yield SplashRequest(url=url, callback=self.parse_details)
 
     def parse_details(self, response):
-    	full_text = response.css('div#print').extract_first()
+    	def extract_with_css(query):
+    		return response.css(query).extract_first()
+
+    	full_text = extract_with_css('div#print')
     	soup = BeautifulSoup(full_text, 'html.parser')
-    	yield {
-    		'structured': {
-    			'titulo': response.css('p.aviso-titulo::text').extract_first(),
-    			'sintesis': response.css('p.aviso-sintesis::text').extract_first(),
-    			'fecha': response.css('p.aviso-fecha::text').extract_first(),
-    		},
-    		'full-text': soup.get_text(),
-    	}
+
+    	yield Norma({
+			'title': extract_with_css('p.aviso-titulo::text'),
+			'abstract': extract_with_css('p.aviso-sintesis::text'),
+			'date': extract_with_css('p.aviso-fecha::text'),
+    		'full_text': soup.get_text(),
+    	})
