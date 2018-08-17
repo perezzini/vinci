1e29c6f794f4430d983a3a090fa22a17ae40d837
Luciano Perezzini <lperezzini@gmail.com>
2018-08-17 12:51:30 -0300

    Clean file

--- main.py
+++ main.py
@@ -1,25 +1,9 @@
 # Main module: where code must run
 
-import logging
-from database import DB
-
-logging.basicConfig(filename='.log',
-					level=logging.DEBUG,
-					format='%(asctime)s - %(name)s - %(threadName)s -  %(levelname)s - %(message)s')
-
 print('Main started...')
 print('----------------\n')
 
-mongo_client = DB()
-
-# Do some more work...
-
-if __name__ == "__main__":
-    logging.warning("I'm a warning!")   
-    logging.info("Hello, Python!")
-    logging.debug("I'm a debug message!")
-
-mongo_client.close_conn()
+# Do some work...
 
 print('\n----------------')
 print('Main finished')
