3aa383fa9bf1886d9d306d16ffd7021e8e15c2e8
Luciano Perezzini <lperezzini@gmail.com>
2018-08-18 19:30:20 -0300

    Some errors fix

--- crawler/spiders/bo_nacional.py
+++ crawler/spiders/bo_nacional.py
@@ -17,12 +17,17 @@ class BONacional(scrapy.Spider):
 		norm_urls = norm_items.css('h3 > a::attr(href)').extract()
 		for url in list(norm_urls):
 			url = response.urljoin(url)
-			print(url)
 			yield SplashRequest(url=url, callback=self.parse_details)
 
 	def parse_details(self, response):
 		def extract_with_css(query):
 			return response.css(query).extract_first()
+
+		if extract_with_css('p.aviso-norma::text'):
+			type = NormaNacional.get_type_of_norm(extract_with_css('p.aviso-norma::text'))
+		else:
+			type = 'None'
+
 
 		full_text = extract_with_css('div#print')
 		soup = BeautifulSoup(full_text, 'html.parser')
@@ -32,4 +37,5 @@ class BONacional(scrapy.Spider):
 			'abstract': extract_with_css('p.aviso-sintesis::text'),
 			'date': extract_with_css('p.aviso-fecha::text'),
 			'full_text': soup.get_text(),
+			'type': type,
 		})
