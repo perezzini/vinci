b8de63798247b16d20b4b5626c190e2bbcbff226
Luciano Perezzini <lperezzini@gmail.com>
2018-08-19 18:43:15 -0300

    Remove spider 'quotes_js'

--- crawler/items.py
+++ crawler/items.py
@@ -7,13 +7,6 @@
 
 import scrapy
 
-class Quote(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    text = scrapy.Field()
-    author = scrapy.Field()
-    tags = scrapy.Field()
-
 class Author(scrapy.Item):
 	description = scrapy.Field()
 
