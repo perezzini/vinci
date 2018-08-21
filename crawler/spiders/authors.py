90522ebbe0eb517e6b93ba2c40637ab9fd27508e
Luciano Perezzini <lperezzini@gmail.com>
2018-08-21 15:43:36 -0300

    Use Lua script

--- crawler/spiders/authors.py
+++ crawler/spiders/authors.py
@@ -6,15 +6,40 @@ from crawler.items import Author
 class Authors(scrapy.Spider):
     name = 'authors'
 
+    lua_script = """
+                function main(splash)
+                  splash.private_mode_enabled = true
+                  local url = splash.args.url
+                  assert(splash:go(url))
+                  assert(splash:wait(10))
+                  return {
+                    html = splash:html(),
+                    har = splash:har(),
+                  }
+                end
+                """  # TODO: must better understand this...
+
     def start_requests(self):
         url = 'http://quotes.toscrape.com/'
-        yield SplashRequest(url=url, callback=self.parse)
+        yield SplashRequest(url=url,
+                            callback=self.parse,
+                            endpoint='execute',
+                            args={
+                                    'lua_source': self.lua_script,
+                                    'wait': 5,
+                                })
 
     def parse(self, response):
     	urls = response.css('div.quote > span > a::attr(href)').extract()
     	for url in urls:
     		url = response.urljoin(url)
-    		yield SplashRequest(url=url, callback=self.parse_details)
+    		yield SplashRequest(url=url,
+                                callback=self.parse_details,
+                                endpoint='execute',
+                                args={
+                                    'lua_source': self.lua_script,
+                                    'wait': 5,
+                                })
 
     def parse_details(self, response):
         container = response.css('div.author-details').extract_first()
