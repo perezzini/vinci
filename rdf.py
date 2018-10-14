109bb9ea1c8f47b07903a22eb563b4e610cb7eae
Luciano Perezzini <lperezzini@gmail.com>
2018-10-14 20:28:08 -0300

    Wrapper of some SKOS functionalities

--- rdf.py
+++ rdf.py
@@ -1,5 +1,8 @@
 from rdflib import Graph, Literal
 from rdflib.namespace import SKOS as SKS
+from rdflib.plugins.sparql import prepareQuery
+
+from graph import DiGraph
 
 class RDF():
     def __init__(self, file_path):
@@ -9,31 +12,62 @@ class RDF():
     def graph_len(self):
         return len(self.graph)
 
-    def get_objects(self, subject, predicate):
-        l = list()
-        for obj in self.graph.objects(subject, predicate):
-            l.append(obj)
-        return l
+    def literal_to_text(self, lit):
+        return lit.toPython()
 
-    def object_exists(self, object):
-        if object in self.get_objects(self, None, None):
-            return True
-        else:
-            return False
+    def query(self, q):
+        return self.graph.query(q)
 
-    def get_subject(self, predicate, object):
-        if object_exists(self, object):
-            return self.graph.subjects(predicate, object)
-        else:
-            raise Exception('Object does not exist in graph')
+    def get_preferred_label(self, uri):
+        return self.graph.preferredLabel(uri)
+
+    def triples(self, subject, predicate, object):
+        return self.graph.triples((subject, predicate, object))
 
 class SKOS(RDF):
     def __init__(self, vocab_file):
         self.vocab = RDF(vocab_file)
+        self.terms = self.get_terms()
+        self.properties = ['prefLabel', 'narrower', 'broader', 'related']
 
-    def get_literal_terms(self):
-        objects = self.vocab.get_objects(None, SKS['prefLabel'])
-        return list(map(lambda lit: lit.toPython(), objects))
+    def get_terms_with_uris(self):
+        q = prepareQuery('SELECT ?concept ?term WHERE { ?concept skos:prefLabel ?term .}', initNs = { 'skos': SKS })
+        l = list()
+        for row in self.vocab.query(q):
+            l.append(row)
+        return l
+
+    def get_terms(self):
+        l = self.get_terms_with_uris()
+        l = list(map(lambda pair: self.vocab.literal_to_text(pair[1]), l))
+        return [term.lower() for term in l]
 
     def get_num_of_terms(self):
-        return len(self.get_literal_terms())
+        return len(self.terms)
+
+    def term_exists(self, term):
+        term = term.lower()
+        if term in self.terms:
+            return True
+        else:
+            return False
+
+    def get_uri_from_term(self, term):
+        if self.term_exists(term):
+            term = term.lower()
+            l = list(filter(lambda pair: self.literal_to_text(pair[1]) == term, self.get_terms_with_uris()))
+            return list(map(lambda pair: pair[0], l))[0]
+        else:
+            raise Exception(term + ' does not exist in opened vocabulary')
+
+    def get_term_from_uri(self, uri):
+        label = self.vocab.get_preferred_label(uri)[0][1]
+        return self.literal_to_text(label).lower()
+
+    def search_tree(self, property, node):
+        if property not in self.properties:
+            raise Exception(property + ' is not a SKOS property')
+        di_graph = DiGraph()
+        for s, p, o in self.vocab.triples(None, SKS[property], None):
+            di_graph.add_edge(s, o)
+        return list(map(lambda uri: self.get_term_from_uri(uri), di_graph.dfs_tree(node)))
