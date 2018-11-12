585b816c2de84d055b5604461e0e113126e76d1e
Luciano Perezzini <lperezzini@gmail.com>
2018-11-11 22:38:33 -0300

    Del NLP seperated tasks

--- nlp/probability.py
+++
@@ -1,9 +0,0 @@
-from nltk.probability import FreqDist
-from nlp.normalization import Normalization
-
-class Probability():
-    def __init__(self, tokens):
-        self.tokens = tokens
-
-    def freq_dist(self):
-        return FreqDist(self.tokens)
