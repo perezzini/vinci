9a3de53e01004f06bc8241c86741cd9943ccbfde
Luciano Perezzini <lperezzini@gmail.com>
2018-11-17 20:35:35 -0300

    New settings

--- settings.py
+++ settings.py
@@ -4,6 +4,10 @@ from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
 # Retrieve database config
-DB_SERVER = os.getenv("DB_SERVER")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
+DB_SERVER = os.getenv('DB_SERVER')
+DB_PORT = os.getenv('DB_PORT')
+DB_NAME = os.getenv('DB_NAME')
+
+# Legal terms vocabulary path
+LEGAL_VOC_RDF_PATH = os.getenv('LEGAL_VOC_RDF_PATH')
+LEGAL_VOC_API_PATH = os.getenv('LEGAL_VOC_API_PATH')
