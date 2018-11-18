26ee8cc182b2304317826a6f2c5137c1524c30b8
Luciano Perezzini <lperezzini@gmail.com>
2018-11-17 21:05:19 -0300

    Update

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
+from database import DB
+
 # Main module: where code must run
 
 print('Main started...')
 print('----------------\n')
-
-# Do some work...
 
 print('\n----------------')
 print('Main finished')
