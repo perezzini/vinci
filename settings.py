51c62713fc60cc73bdf537d029be8b48f499b350
Luciano Perezzini <lperezzini@gmail.com>
2018-11-26 11:56:45 -0300

    Add lemmas config

--- settings.py
+++ settings.py
@@ -8,6 +8,9 @@ DB_SERVER = os.getenv('DB_SERVER')
 DB_PORT = os.getenv('DB_PORT')
 DB_NAME = os.getenv('DB_NAME')
 
+# Lemmas
+ES_LEMMAS_PATH = os.getenv('ES_LEMMAS_PATH')
+
 # Legal terms vocabulary path
 LEGAL_VOC_RDF_PATH = os.getenv('LEGAL_VOC_RDF_PATH')
 LEGAL_VOC_API_PATH = os.getenv('LEGAL_VOC_API_PATH')
