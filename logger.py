0d033b5fee79af8a4d5b048969706ebff17c71ba
Luciano Perezzini <lperezzini@gmail.com>
2019-01-03 15:15:42 -0300

    Poor update

--- logger.py
+++ logger.py
@@ -5,6 +5,15 @@ class Log():
 		logging.basicConfig(filename='.log',
 							level=logging.DEBUG,
 							format='%(asctime)s - %(name)s - %(threadName)s -  %(levelname)s - %(message)s')
+		# define a Handler which writes INFO messages or higher to the sys.stderr
+		console = logging.StreamHandler()
+		console.setLevel(logging.DEBUG)
+		# set a format which is simpler for console use
+		formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+		# tell the handler to use this format
+		console.setFormatter(formatter)
+		# add the handler to the root logger
+		logging.getLogger('').addHandler(console)
 
 	def warning(self, msg):
 		logging.warning(msg)
