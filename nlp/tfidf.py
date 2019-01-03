c8797a1dccd910276bf6f2058e100cadaeffb2d5
Luciano Perezzini <lperezzini@gmail.com>
2019-01-03 15:14:26 -0300

    Poor update

--- nlp/tfidf.py
+++ nlp/tfidf.py
@@ -1,33 +1,31 @@
 from gensim.corpora import Dictionary
 from gensim.models import TfidfModel
 from gensim import similarities
+from gensim.utils import SaveLoad
+
+from nlp.preprocess import Preprocess
 
 from heapq import nlargest
 
 class Tfidf():
-    def __init__(self, dataset):
-        self.dataset = dataset  # pre-processed data
-        self.dict = Dictionary(self.dataset)
-        self.corpus = [self.dict.doc2bow(line) for line in dataset]
+    def __init__(self, lang, dict_path, model_path, sim_matrix_path):
+        self.text_preproc = Preprocess(lang)
+        self.dict = Dictionary.load(dict_path)
+        self.model = TfidfModel.load(model_path)
+        self.sim_matrix = SaveLoad.load(sim_matrix_path)
 
-    def number_of_tokens(self):
-        return len(self.dict)
+    def doc_to_vec(self, doc):
+        doc = self.dict.doc2bow(self.text_preproc.complete_process(doc))
+        return self.model[doc]
 
-    def get_token_from_id(self, id):
+    def get_top_words_from_doc(self, doc, n):
+        vec = self.doc_to_vec(doc)
+        return nlargest(n, vec, key=lambda tuple: tuple[1])
+
+    def from_dict(self, id):
         return self.dict[id]
 
-    def get_id_from_token(self, token):
-        pass
-
-    def get_corpus(self):
-        return self.corpus
-
-    def get_dataset(self):
-        return self.dataset
-
-    def gen_model(self, normalize=False):
-        self.model = TfidfModel(self.corpus, normalize=normalize)
-        return self.model
-
-    def get_top_tokens(self, vector, n):
-        return nlargest(n, vector, key=lambda tuple: tuple[1])
+    def get_most_similar_doc_id(self, doc):
+        vec = self.doc_to_vec(doc)
+        sim_vec = self.sim_matrix[self.model[vec]]
+        return sim_vec.argmax()  # returns the most similar
