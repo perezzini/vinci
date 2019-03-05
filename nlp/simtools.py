ae4ef86eff90f571d771791037dbf7be9f1fb4f3
Luciano Perezzini <lperezzini@gmail.com>
2019-03-05 17:06:50 -0300

    Define get_most_sim_doc_ids()

--- nlp/simtools.py
+++ nlp/simtools.py
@@ -1,15 +1,18 @@
 from gensim import similarities
-from nlp.dicttools import get_num_of_features
 import os
 from heapq import nlargest
 from gensim import matutils
+from nlp.corpustools import get_num_features
 
-def create_index(model, corpus, dict):
+def create_index(model, corpus):
     """
     Builds an index for a given set of documents for the
     purpouse of computing cosine similarities
+
+    Note: Use this if input corpus contains sparse vectors (such as TF-IDF documents)
+    and fits into RAM
     """
-    return similarities.SparseMatrixSimilarity(model[corpus], num_features=get_num_of_features(dict))
+    return similarities.SparseMatrixSimilarity(model[corpus], num_features=get_num_features(corpus))
 
 def save_index(index, name):
     index.save(os.getenv('SIMS_PATH') + '/' + name + '.index')
@@ -18,15 +21,17 @@ def query_index(q, index):
     """
     Given a query (doc) and an index of documents (cosine-sim-ready),
     computes the cosine similarity between the query and every single
-    doc in the index
+    doc in the index (searches documents similar to query in index)
     """
     return index[q]
 
-def get_largest_doc_ids(sims, n):
+def get_most_sim_doc_ids(q, n, index):
     """
-    Given a computed query-docs similarity,
-    retrieves the n largest similar documents (ids)
+    Returns the N most similar documents to a query in a given collection
+    of documents (indexed docs), using a given transformation model.
+    If N > len(index), returns the every doc in index (in decreasing order)
     """
+    sims = query_index(q, index)
     return nlargest(n, range(len(sims)), sims.take)
 
 def cos_sim(vec1, vec2):
